21c7a2c707899f9cd54240d4d7f7447563f04cef
bcn-alumno <bcn-alumno@nm_madrid.local>
2020-12-17 13:24:15 +0100

    done

--- js/app.js
+++ js/app.js
@@ -1,23 +1,24 @@
-// Tu código aquí
-
+(function () {
+/* ==== Select the input element ==== */
 const form = document.querySelector('#message-form')
-const button = document.querySelector('#submitBtn');
-const messageContent = document.querySelector('.message-content');
-const information = document.querySelector('information');
+/* ==== Set up Submit Button ==== */
 
 form.addEventListener('submit', function(e){
-    console.log("Información sobre el objeto event: ", e)
     e.preventDefault()
-    messageContent.textContent = document.querySelector('#message').value;
+    const messageContent = document.querySelector('.message-content');
+    const feedback = document.querySelector('.feedback');
+    const message = document.querySelector('#message');
 
+    messageContent.textContent = message.value;
+    
     if(document.querySelector('#message').value == '') {
-       
-            let errorMessage = document.createElement('H5');
-            let errorMessageText = document.createTextNode("please enter a value to pass");
-            errorMessage.appendChild(errorMessageText);
-            document.querySelector('.information').appendChild(errorMessage);
-
+        feedback.classList.add('show');
+        setTimeout(function(){
+        feedback.classList.remove('show');
+        }, 2000)
     } else {
-
+        messageContent.textContent = message.value;
+        message.value = '';
     }
 })
+})()
