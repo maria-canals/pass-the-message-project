f98aa9ea1aa791c4c80017849c3725a41c867a16
bcn-alumno <bcn-alumno@nm_madrid.local>
2020-12-17 13:41:16 +0100

    finished

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 
-/* ==== Select the input element ==== */
-const form = document.querySelector('#message-form')
-/* ==== Set up Submit Button ==== */
+
+const form = document.querySelector('#message-form');
+
 
 form.addEventListener('submit', function(e){
-    e.preventDefault()
+    e.preventDefault();
     const messageContent = document.querySelector('.message-content');
     const feedback = document.querySelector('.feedback');
     const message = document.querySelector('#message');
 
-    if(document.querySelector('#message').value == '') {
+    if(message.value == '') {
         feedback.classList.add('show');
         setTimeout( () => {
-        feedback.classList.remove('show')
+        feedback.classList.remove('show');
         }, 2000);
     } else {
         messageContent.textContent = message.value;
     }
-})
+});
